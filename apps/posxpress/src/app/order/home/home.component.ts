8758840f51107ad598a6191dc3a53453c0446c1f
schlaumra <schlaumra@gmail.com>
2024-05-28 21:28:29 +0200

    feat: create logout button

--- apps/posxpress/src/app/order/home/home.component.ts
+++ apps/posxpress/src/app/order/home/home.component.ts
@@ -5,18 +5,20 @@ import { MatIconModule } from '@angular/material/icon';
 import { AuthService } from '../../auth/auth.service';
 import { OrderService } from '../order.service';
 import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'px-home',
   templateUrl: 'home.component.html',
   styleUrls: ['home.component.scss'],
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, NgFor, AsyncPipe, MatCardModule, NgIf],
+  imports: [MatButtonModule, MatIconModule, NgFor, AsyncPipe, MatCardModule, NgIf, MatDividerModule],
 })
 export class HomeComponent {
   user: string;
 
-  constructor(private authService: AuthService, protected orderService: OrderService) {
+  constructor(private authService: AuthService, protected orderService: OrderService, private router: Router) {
     this.user = this.authService.getUser();
     this.orderService.orderStore.clearCurrentOrder()
   }
@@ -27,5 +29,9 @@ export class HomeComponent {
 
   removeOrder(table: number) {
     this.orderService.orderStore.removeOrder(table)
+  }
+
+  logout() {
+    this.router.navigate(['logout'])
   }
 }
